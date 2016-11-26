35ba3c38a1648159e740941ad8f56da426e60680
YangDong <coolyoungever@foxmail.com>
2016-11-26 18:09:57 +0800

    Initial Commit

---
+++ YHDrawBoardDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHDrawBoardDemo
+//
+//  Created by YHCloud on 2016/11/26.
+//  Copyright © 2016年 YHCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHDrawBoardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHDrawBoardDemo
+//
+//  Created by YHCloud on 2016/11/26.
+//  Copyright © 2016年 YHCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
